d0f66967b4b554f5e1e6aeabd82ab08401e02294
宇根昇汰 <une@unenoborunoMacBook-Air.local>
2023-02-13 03:41:15 +0900

    データベース操作を追加（途中）

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const pg = require('pg');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const client = new pg.Client({
+    user: 'une',
+    host: 'localhost',
+    database: 'login',
+    password: 'llsyou',
+    port: 5432
+})
+
+client.connect()
+// const query = {
+//     text: 'SELECT USER_NAME FROM REGISTERED_USERS;',
+// }
+const query = client.query('SELECT * FROM REGISTERED_USERS WHERE USER_NAME = $1;')
+
+
 app.post('/auth', function(req, res) {
     if(req.body.id == "une" && req.body.pass == "unko"){
         res.send({
-            message: "OK"
+            message: "OK",
+            query: query
         })
     }else{
         res.send({
